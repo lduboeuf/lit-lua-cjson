8cd47f90f54dc78b588ac715bad01098240b4403
lduboeuf <lionel.duboeuf@marvinbot.com>
2015-05-13 15:17:32 +0200

    add authors

--- package.lua
+++ package.lua
@@ -1,11 +1,13 @@
 return {
   name = "lduboeuf/cjson",
-  version = "0.5.0",
+  version = "1.0.1",
   homepage = "https://github.com/lduboeuf/lit-lua-cjson",
   description = "lit wrapper for Mark Pulford's lua-cjson library",
   tags = {"json", "codec"},
   license = "MIT",
-	authors = "Lionel Duboeuf",
+	 exports.author = {
+  	name = "Lionel Duboeuf"
+	},
   files = {
     "*.lua",
     "$OS-$ARCH/*",
