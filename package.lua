b3b02cc01714d825bb3f074bd10dda694074ec80
lduboeuf <lionel.duboeuf@gmail.com>
2015-07-24 23:29:03 +0200

    correct lit metadatas & version

--- package.lua
+++ package.lua
@@ -1,13 +1,16 @@
 return {
   name = "lduboeuf/cjson",
-  version = "1.0.1-1",
+  version = "0.0.1",
   homepage = "https://github.com/lduboeuf/lit-lua-cjson",
   description = "lit wrapper for Mark Pulford's lua-cjson library",
   tags = {"json", "codec"},
   license = "MIT",
 	author = {
-  	name = "Lionel Duboeuf"
+  	name = "Mark Pulford"
 	},
+  contributors = {
+    "Lionel Duboeuf"
+  },
   files = {
     "*.lua",
     "$OS-$ARCH/*",
